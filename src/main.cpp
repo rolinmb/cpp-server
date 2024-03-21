a4f7497dea3b63666c6400ec5fcb379b95b1a40c
Rolin Blake <rolinmblake@gmail.com>
2024-03-21 06:11:15 -0500

    added simple server side routing via window.location.href calls on client HTML content

--- src/main.cpp
+++ src/main.cpp
@@ -8,12 +8,15 @@
 #include <Windows.h>
 #include <fstream>
 #include <string>
+#include <algorithm>
+#include <iostream>
+#include <cstring>
 
 #define PORT 8080
 
 const char* szResponse = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n";
 const char* notFoundResponse = "HTTP/1.1 404 Not Found\r\n\r\n";
-const char szHtml[] = R"(
+const char szHtmlIndex[] = R"(
 <!DOCTYPE HTML>
 <html>
   <head>
@@ -22,6 +25,7 @@ const char szHtml[] = R"(
   <body>
     <h1>This HTML content is the server response from main.cpp</h1>
     <button id="click-btn"></button>
+	<button id="nav-btn">Go to other content</button>
   </body>
   <script>
     var count = 0;
@@ -34,7 +38,31 @@ const char szHtml[] = R"(
       let btn = document.getElementById("click-btn");
       btn.innerHTML = count.toString();
       btn.addEventListener("click", handleClick);
+	  let navbtn = document.getElementById("nav-btn");
+	  navbtn.addEventListener("click", function() {
+	    window.location.href = "http://localhost:8080/other";
+	  });
     };
+  </script>
+</html>
+)";
+const char szHtmlOther[] = R"(
+<!DOCTYPE HTML>
+<hmtl>
+  <head>
+    <title>Server Response From main.cpp</title>
+  </head>
+  <body>
+    <h1>This is some other content you have been routed to.</h1>
+	<button id="nav-btn">Go to the home page</button>
+  </body>
+  <script>
+    window.onload = function() {
+	  let btn = document.getElementById("nav-btn");
+	  btn.addEventListener("click", function() {
+	    window.location.href = "http://localhost:8080/";
+	  });
+	};
   </script>
 </html>
 )";
@@ -78,12 +106,26 @@ int main(const int argc, const char* argv[]) {
     }
 	printf("main.cpp :: Server has received raw client request\n");
     std::string httpRequest(buffer, bytesReceived);
-    if (httpRequest.find("GET / HTTP") != std::string::npos) {
-      send(clientSocket, szResponse, strlen(szResponse), 0);
-      send(clientSocket, szHtml, strlen(szHtml), 0);
-    } else {
-      send(clientSocket, notFoundResponse, strlen(notFoundResponse), 0);
-    }
+	printf("main.cpp :: Raw Client HTTP Request:\n%s\n", httpRequest.c_str());
+	std::string path;
+	size_t start = httpRequest.find("GET ");
+    if (start != std::string::npos) {
+	  start += 4;
+	  size_t end = httpRequest.find(' ', start);
+	  if (end != std::string::npos) {
+		path = httpRequest.substr(start, end - start);
+	  }
+	}
+	std::transform(path.begin(), path.end(), path.begin(), ::tolower);
+	if (path == "/" || path == "/index.html" || path == "index" || path == "home") {
+	  send(clientSocket, szResponse, strlen(szResponse), 0);
+	  send(clientSocket, szHtmlIndex, strlen(szHtmlIndex), 0);
+	} else if (path == "/other") {
+	  send(clientSocket, szResponse, strlen(szResponse), 0);
+	  send(clientSocket, szHtmlOther, strlen(szHtmlOther), 0);
+	} else {
+	  send(clientSocket, notFoundResponse, strlen(notFoundResponse), 0);
+	}
     printf("main.cpp :: Server has sent HTML response to client GET request\n");
     fflush(stdout);
     closesocket(clientSocket);
